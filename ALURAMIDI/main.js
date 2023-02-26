af5254338e1c9710e3162dff62caf965c004704d
kevincamussi <111162369+kevincamussi@users.noreply.github.com>
2023-02-26 20:34:38 +0000

    Add files via upload

---
+++ ALURAMIDI/main.js
@@ -0,0 +1,19 @@
+const teclas = document.querySelectorAll('.tecla');
+document.querySelector('.teclado').onload = tocarSom();
+
+let som = new Audio();
+
+function tocarSom(){
+teclas.forEach(tecla => {
+  const instrumentos = tecla.dataset.tecla;
+  
+  tecla.addEventListener('click',()=>{
+    som.src=`sounds/${instrumentos}.wav`;
+    som.play();    
+   
+  })
+
+});
+
+}
+
